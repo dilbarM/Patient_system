ec22e4f825ecedcad40c95b6ed3a3647df5875f7
dilbarM <ddilbar100@gmail.com>
2025-05-26 18:54:55 +0530

    updated layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,6 @@ import Sidebar from './Sidebar';
 const Layout: React.FC = () => {
   const [sidebarOpen, setSidebarOpen] = React.useState(false);
 
-  
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <Navbar onMenuClick={() => setSidebarOpen(true)} />
